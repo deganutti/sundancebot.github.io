508fca127b17147a94d764500a3adf160d04ce7b
deganutti <deganutti@outlook.com.br>
2020-07-03 10:59:50 -0300

    Criado arquibo grade.js controle de grades do game

---
+++ src/commands/grade.js
@@ -0,0 +1,70 @@
+const MessageEmbed = require("discord.js").MessageEmbed;
+const execute = (bot, msg, args) => {
+
+    if (args.length === 0) {
+        const embed = new MessageEmbed()
+            .setAuthor(`Sundance`, "https://i.pinimg.com/originals/40/8a/38/408a38dcd7a86586ac5eb1283e9910e2.gif", "https://deganutti.github.io")
+            .setColor(process.env.COR_EMBED)
+            .setTitle("Iniciando assistente desafio de Osiris!")
+            .setDescription("Selecione a opção desejada")
+            .addFields([
+                { name: "Novo", value: "✳️" },
+                { name: "Entrar", value: "🔫" },
+                { name: "Sair", value: "❌" }
+            ]);
+        msg.member.send({ embed }).then(async (embed) => {
+            try {
+                await embed.react("✳️");
+                await embed.react("🔫");
+                await embed.react("❌");
+                const collector = embed.createReactionCollector((reaction, user) =>
+                    [
+                        "✳️",
+                        "🔫",
+                        "❌",
+                    ].includes(reaction.emoji.name) && !user.bot,
+                    {
+                        time: 5000,
+                    }
+                );
+                collector.on("collect", (reaction, user) => {
+                    switch (reaction.emoji.name) {
+                        case "✳️":
+                            try {
+
+                                msg.member.send(`\`Você entrou no pareamento\``);
+                                console.log("Ação -> " + reaction.emoji.name);
+                            } catch (error) {
+                                console.error("Error " + e);
+                            }
+                            break;
+                        case "🔫":
+                            try {
+
+                                console.log("Ação -> " + reaction.emoji.name);
+                            } catch (error) {
+                                console.error("Error " + e);
+                            }
+                            break;
+                        case "❌":
+                            try {
+
+                                console.log("Ação -> " + reaction.emoji.name);
+                            } catch (error) {
+                                console.error("Error " + e);
+                            }
+                            break;
+                    }
+                })
+            } catch (e) {
+                console.error("Atenção => Erro encontrado " + e);
+            }
+        });
+        msg.channel.bulkDelete(1);
+    }
+}
+module.exports = {
+    name: "grade",
+    ajuda: "Faz a manutenção da Grade referente ao Destiny 2.",
+    execute
+}
